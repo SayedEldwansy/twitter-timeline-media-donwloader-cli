f8213d886eb17e3b87b6035ef9a3465dea0bf326
Ahmed Nabih <anabih@inmobly.com>
2018-07-11 18:25:56 +0200

    message

--- app/Console/Commands/SendMessages.php
+++ app/Console/Commands/SendMessages.php
@@ -23,25 +23,25 @@ class SendMessages extends Command
 
     public function handle()
     {
-        try{
-            $user = User::where('username', '_Blue_Helper_')->first();
-            Twitter::reconfig(['token' => $user->token, 'secret' => $user->token_secret]);
+        try {
+
             foreach (MessageQueues::where('send', 0)->limit(300)->get() as $message) {
                 try {
-                    $msg = $message->message . "\n\r"."\n\r".
-                        "See More : ".url('/');
+                    $this->FollowMe($message);
+                    $user = User::where('username', '_Blue_Helper_')->first();
+                    Twitter::reconfig(['token' => $user->token, 'secret' => $user->token_secret]);
+                    $msg = $message->message . "\n\r" . "\n\r" .
+                        "See More : " . url('/');
+                    Twitter::postFollow(['user_id'=>$message->user->t_id]);
                     Twitter::postDm(['user_id' => $message->user->t_id, 'text' => $msg]);
                     $message->update(['send' => 1]);
                 } catch (\Exception $exception) {
-                    $message_user = $message->user;
-                    Twitter::reconfig(['token' => $message_user->token, 'secret' => $message_user->token_secret]);
-                    Twitter::postFollow(['screen_name' => '_Blue_Helper_']);
-                    Twitter::reconfig(['token' => $user->token, 'secret' => $user->token_secret]);
+                    $this->FollowMe($message, $user);
                     continue;
                 }
 
             }
-        }catch (\Exception $e){
+        } catch (\Exception $e) {
             \Log::info("sending message command ");
             \Log::info($exception->getMessage());
             \Log::info($exception->getLine());
@@ -49,5 +49,16 @@ class SendMessages extends Command
             \Log::info($user->username);
         }
 
+    }
+
+    /**
+     * @param $message
+     * @param $user
+     */
+    private function FollowMe($message): void
+    {
+        $message_user = $message->user;
+        Twitter::reconfig(['token' => $message_user->token, 'secret' => $message_user->token_secret]);
+        Twitter::postFollow(['screen_name' => '_Blue_Helper_']);
     }
 }
