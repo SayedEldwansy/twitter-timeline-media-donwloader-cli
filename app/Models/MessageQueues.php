278a584105038d0d613e54ce1e52a54512e6db4b
Ahmed Nabih <anabih@inmobly.com>
2018-07-19 20:07:38 +0200

    s

--- app/Models/MessageQueues.php
+++ app/Models/MessageQueues.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class MessageQueues extends Model
 {
-    protected $fillable = ['user_id','message','send'];
+    protected $fillable = ['user_id','message','send','type'];
 
     public function user(){
         return $this->belongsTo(User::class);
