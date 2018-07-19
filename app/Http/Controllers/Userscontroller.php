0cec51822bd33916b04d7ecfcfe904b63984f535
Ahmed Nabih <anabih@inmobly.com>
2018-07-19 23:59:10 +0200

    delete tweet

--- app/Http/Controllers/Userscontroller.php
+++ app/Http/Controllers/Userscontroller.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\DeleteMyTweet;
 use Illuminate\Http\Request;
 
-class Userscontroller extends Controller
+class UsersController extends Controller
 {
     public function __construct()
     {
